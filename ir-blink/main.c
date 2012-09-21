073845737d11040216b5bb6bacc47152369927d4
Sam Jacoby <sjacoby@media.mit.edu>
2012-09-21 07:57:35 -0400

    Wherever we are, we are.

--- ir-blink/main.c
+++ ir-blink/main.c
@@ -14,24 +14,46 @@ void setup(void) {
     CLKPR = (1 << CLKPCE); 
     CLKPR = 0;
 
+    // CONFIGURE TIMER0
     // Configure 38Khz
     // Run the timer w/ a prescale
     TCCR0B = 1 << CS01;
 
-    OCR0A = 53;         // regulates frequency. some number.
-    OCR0B = 53;  // constant duty-cucle of 50% 
+    OCR0A = 50;     // regulates frequency. some number.
+    OCR0B = 25;     // constant duty-cucle of 50% 
 
     TCCR0A |= (1 << WGM01 ) | (1 << COM0B0);
 
     // Enable interrupts in the right places
     TIMSK0 = 1 << OCIE0B;
 
-    DDRD |= 1 << PD5;
 
+    // CONFIGURE TIMER1
+    TCCR1A = (1 << WGM12); // CTC on OCR1A
+    TCCR1B = (1 << CS12);
+
+    OCR1AH = 0x0e; 
+    OCR1AL = 0xa6;
+
+    TIMSK1 = (1 << OCIE1A); // Interrupt enable
+    
+}
+
+volatile uint16_t ir_count;
+ISR(TIM1_COMPA) {
+    static uint16_t count;
+    count = (count + 1) & 0xffff; // rollover
+
+    if(count < ir_count) { 
+        DDRD |= 1 << PD5;
+    } else {
+        DDRD &= ~(1 << PD5); 
+    }
 }
 
 
 void loop() {
+   ir_count = 1024; 
 }
 
 int main(void) {
