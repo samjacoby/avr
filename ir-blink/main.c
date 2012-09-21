45e51064364a4bd9e53f1102bf1cf60650bc1f90
Sam Jacoby <sjacoby@media.mit.edu>
2012-09-21 18:04:40 -0400

    Adding some things.

--- ir-blink/main.c
+++ ir-blink/main.c
@@ -18,6 +18,9 @@ void setup(void) {
     CLKPR = (1 << CLKPCE); 
     CLKPR = 0;
 
+    // Set-up serial
+    DDRD |= 1 << PD1;
+
     // CONFIGURE TIMER0
     // Configure 38Khz
     // Run the timer w/ a prescale
@@ -41,6 +44,46 @@ void setup(void) {
 
 }
 
+#define F_CPU 16000000	// 16 MHz oscillator.
+#define BaudRate 9600
+#define MYUBRR (F_CPU / 16 / BaudRate ) - 1 
+
+/* Serial Funcs */
+void serialInit() {
+ 	/*Set baud rate */ 
+	UBRR0H = (unsigned char)(MYUBRR>>8); 
+	UBRR0L = (unsigned char) MYUBRR; 
+	/* Enable receiver and transmitter   */
+	UCSR0B = (1<<RXEN0)|(1<<TXEN0); 
+	/* Frame format: 8data, No parity, 1stop bit */ 
+	UCSR0C = (3<<UCSZ00);  
+
+}
+
+unsigned char serialCheckRxComplete(void)
+{
+	return( UCSR0A & _BV(RXC0)) ;		// nonzero if serial data is available to read.
+}
+
+unsigned char serialCheckTxReady(void)
+{
+	return( UCSR0A & _BV(UDRE0) ) ;		// nonzero if transmit register is ready to receive new data.
+}
+
+unsigned char serialRead(void)
+{
+	while (serialCheckRxComplete() == 0)		// While data is NOT available to read
+	{;;} 
+	return UDR0;
+}
+
+void serialWrite(unsigned char DataOut)
+{
+	while (serialCheckTxReady() == 0)		// while NOT ready to transmit 
+	{;;} 
+	UDR0 = DataOut;
+}
+
 
 
 ISR(TIMER1_COMPA_vect) {
@@ -62,26 +105,35 @@ ISR(TIMER1_COMPA_vect) {
 
 void loop() {
     sei();
-    int i;
+    int i, inByte;
     for(i = 0; i < 64; i++) {
         ir_count = i;
-        _delay_ms(10);
+        _delay_ms(15);
     }
+    _delay_ms(220);
     for(i = 63; i >= 0; i--) {
         ir_count = i;
-        _delay_ms(10);
+        _delay_ms(15);
     }
-    _delay_ms(120);
+    _delay_ms(60);
+
+	if (serialCheckRxComplete()) {
+		PORTB |= _BV(1); // Turn on LED @ PB1
+		inByte = serialRead();
+        PORTB &= !_BV(1);
+    }
 }
 
 int main(void) {
 
     setup();
-
-    while(1) {
+    serialInit();
+while(1) {
         loop(); 
     }
     
     return 0;
 }
+
+
 
